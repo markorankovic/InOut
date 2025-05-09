2f554ef13c25f4f03ff77edd15fc3be2fff4a7e7
Marko Ranković <marko@markorankovic.com>
2025-05-09 19:10:14 +0100

    Use obtained ID token

--- App.tsx
+++ App.tsx
@@ -40,8 +40,8 @@ function LoginView(props: { login: boolean, idToken: string | undefined, setIdTo
     logout(
       config,
       {
-        postLogoutRedirectUrl: "https://polite-desert-0d653561e.6.azurestaticapps.net",
-        idToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSJ9.eyJhdWQiOiI3YmU4MWE1Ni0xZGJkLTQ4MTctYjY2Mi1mOTQ5OGI3NGZkNGIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZTliN2MzY2EtZTViOS00ZTM2LTk1MDUtMzE0OTI2NzVkM2FhL3YyLjAiLCJpYXQiOjE3NDY4MDk5ODUsIm5iZiI6MTc0NjgwOTk4NSwiZXhwIjoxNzQ2ODEzODg1LCJlbWFpbCI6InJheXNoYXc3NDM1QGdtYWlsLmNvbSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzkxODgwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZC8iLCJuYW1lIjoiTWFya28gUmFua292aWMiLCJub25jZSI6InFBMzBJS3pVYlJIbHA1SUU1T195R1EiLCJvaWQiOiI5YWRjY2I1Mi1kZDhhLTQ1YzYtYWExMC1jMmE1YzUyNTRkNjYiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJyYXlzaGF3NzQzNUBnbWFpbC5jb20iLCJyaCI6IjEuQVU0QXlzTzM2Ym5sTms2VkJURkpKblhUcWxZYTZIdTlIUmRJdG1MNVNZdDBfVXNXQVJOT0FBLiIsInNpZCI6IjAwNGRiMmI5LTg2NjEtYzRjMC00ODlhLWU4ZGFkZjRjMGE2YSIsInN1YiI6IkpVRWpqVGF6Y0IzUWplUmN3WEt4RURaVTN3dFBETzF5MjVfdmtxb1VLMWciLCJ0aWQiOiJlOWI3YzNjYS1lNWI5LTRlMzYtOTUwNS0zMTQ5MjY3NWQzYWEiLCJ1dGkiOiJpY0EyWXlDMWtrLUU0ZDZKa3JjU0FBIiwidmVyIjoiMi4wIn0.PmLqkODAQnajjDEPRtWhmlU29qpz7Hm52Xfzk2EWj6Ny5S1DCPTnLEZ8KcpMjR6bMMoCkPn5XnMI_ulREmH_KGqzpvH-dz8POJYZ25CS_i8JrRJBkJDHvzIi6RiBzMxEUTFC9K6y-VVnziOWb4ac6qDkuFVrxIqUHVFUiEB_WPoc-XDdgYfueE7-DiasinNKJ69tw3Ech-H-gsrtnciyUcLXxYJ0sSwLPxY4564GR1R_lb3DJBpGl5QnqeIwIQVYYw6JjkLByp0Cyom7hU_IGt9N8IhEsU2rceKPLaMUHksKgWZj8DxVfwk5OZdnkk_WUMw2SFKMRJkZD2Q9RKzOYw"
+        postLogoutRedirectUrl: 'https://polite-desert-0d653561e.6.azurestaticapps.net',
+        idToken: props.idToken!,
       },
     ).then(res => setLogoutStatus(res.state)).catch(err => setLogoutStatus(err));
     props.setIdToken(undefined);
